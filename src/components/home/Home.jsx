2eefd89eea817610b9f1a2c0712a2c464d602a99
Sardorbek Olimov <sardor.20070731@gmail.com>
2023-12-27 22:53:55 +0500

    added top btn

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 function Home() {
   const [select, setSelect] = useState("");
   const [search, setSearch] = useState("");
+  const [scrollY, setScrollY] = useState(false);
 
   const { data, isPending, error } = useFetch(
     select
@@ -18,8 +19,40 @@ function Home() {
       : "https://restcountries.com/v3.1/all"
   );
 
+  const scrollTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      setScrollY(true);
+    } else {
+      setScrollY(false);
+    }
+  });
+
   return (
     <>
+      <div
+        className={scrollY ? "top active" : "top"}
+        onClick={() => {
+          scrollTop();
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-chevron-up"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+          />
+        </svg>
+      </div>
       <Search select={select} setSelect={setSelect} setSearch={setSearch} />
       <Cards data={data} search={search} />
 
