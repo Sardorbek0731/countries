71fc3eb44ea2410cf5af65903b422a038ff4ac80
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-01-03 15:33:57 +0500

    repairing mistakes

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -10,26 +10,28 @@ function Cards({ data, search }) {
         data.map((item, i) => {
           return (
             item.name.common.toLowerCase().includes(search.toLowerCase()) && (
-              <div className="cardItem" key={i}>
-                <NavLink to={item.name.common}>
+              <NavLink to={item.name.common} key={i}>
+                <div className="cardItem">
                   <img src={item.flags.svg} alt={item.flags.alt} />
                   <div className="mainInfo">
                     <h1 className="cardTitle">{item.name.common}</h1>
-                    <h3 className="cardInfo">
-                      <span>Population: </span>
-                      {item.population}
-                    </h3>
-                    <h3 className="cardInfo">
-                      <span>Region: </span>
-                      {item.region}
-                    </h3>
-                    <h3 className="cardInfo">
-                      <span>Capital: </span>
-                      {item.capital}
-                    </h3>
+                    <div className="cardInfos">
+                      <h3 className="cardInfo">
+                        <span>Population: </span>
+                        {item.population}
+                      </h3>
+                      <h3 className="cardInfo">
+                        <span>Region: </span>
+                        {item.region}
+                      </h3>
+                      <h3 className="cardInfo">
+                        <span>Capital: </span>
+                        {item.capital}
+                      </h3>
+                    </div>
                   </div>
-                </NavLink>
-              </div>
+                </div>
+              </NavLink>
             )
           );
         })}
